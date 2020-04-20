3b3e3d5492b8f2e160de743a25a0e33b254d0c29
mariazangelova <mariazangelova@gmail.com>
2020-04-20 17:24:50 +0200

    Import messageReducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import word from "./word/reducer";
 import picture from "./picture/reducer";
+import messageReducer from "./chat/chat";
 
 export default combineReducers({
   word,
   picture,
+  messageReducer,
 });
