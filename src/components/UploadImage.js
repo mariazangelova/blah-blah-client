20c201ddc84003e1fb6338d3d597a00f505230e3
mariazangelova <mariazangelova@gmail.com>
2020-04-22 14:00:31 +0200

    Display the uploaded image

--- src/components/UploadImage.js
+++ src/components/UploadImage.js
@@ -1,61 +1,69 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ImageUploader from "react-images-upload";
 import { Image } from "cloudinary-react";
 import axios from "axios";
 import "../index.scss";
-
-const uploadImage = (file) => {
-  const url = "https://api.cloudinary.com/v1_1/ddag7qg0v/image/upload";
-
-  const body = new FormData();
-  body.append("file", file);
-  body.append("upload_preset", "coifpjwc");
-
-  axios
-    .post(url, body)
-    .then((result) => {
-      console.log(result);
-      const image = result.data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
+const url = "https://api.cloudinary.com/v1_1/ddag7qg0v/image/upload";
 
 export default class UploadImage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { pictures: [] };
+    this.state = {};
     this.onDrop = this.onDrop.bind(this);
+    this.uploadImage = this.uploadImage.bind(this);
   }
 
+  uploadImage(file) {
+    const body = new FormData();
+    body.append("file", file);
+    body.append("upload_preset", "coifpjwc");
+
+    axios
+      .post(url, body)
+      .then((result) => {
+        console.log(result);
+        const image = result.data;
+        this.setState(image);
+        console.log(this.state);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
   onDrop(picture) {
     // this.setState({
     //   pictures: this.state.pictures.concat(picture),
     // });
-    console.log(picture[0]);
-    uploadImage(picture[0]);
+    //console.log(picture[0]);
+    this.uploadImage(picture[0]);
   }
-  // componentDidMount() {
-  //   this.uploadImage()
-  // }
 
   render() {
+    console.log(this.state);
     return (
       <div>
-        <ImageUploader
-          fileContainerStyle={{
-            background: "none",
-            width: "50%",
-            marginTop: "30px",
-          }}
-          withIcon={true}
-          buttonText="Choose images"
-          onChange={this.onDrop}
-          imgExtension={[".jpg", ".gif", ".png", ".gif"]}
-          maxFileSize={5242880}
-        />
-        <Image cloudName="ddag7qg0v" publicId="" width="300" crop="scale" />
+        {Object.keys(this.state).length === 0 ? (
+          <ImageUploader
+            fileContainerStyle={{
+              background: "none",
+              width: "50%",
+              marginTop: "40px",
+            }}
+            withIcon={true}
+            buttonText="Choose images"
+            onChange={this.onDrop}
+            imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+            maxFileSize={5242880}
+          />
+        ) : (
+          <Image
+            cloudName="ddag7qg0v"
+            publicId={this.state.public_id}
+            width="300"
+            style={{ marginTop: "40px" }}
+            crop="scale"
+          />
+        )}
       </div>
     );
   }
