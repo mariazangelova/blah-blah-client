11cde92993f96487e33b1f5ca548df64541f5f4b
mariazangelova <mariazangelova@gmail.com>
2020-04-22 17:40:24 +0200

    Stop recording resets transcript

--- src/components/SpeechRecognition.js
+++ src/components/SpeechRecognition.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 //import { useSelector } from "react-redux";
 import PropTypes from "prop-types";
 import SpeechRecognition from "react-speech-recognition";
@@ -6,6 +6,7 @@ import SpeechRecognition from "react-speech-recognition";
 const propTypes = {
   // Props injected by SpeechRecognition
   transcript: PropTypes.string,
+  finalTranscript: PropTypes.string,
   resetTranscript: PropTypes.func,
   startListening: PropTypes.func,
   stopListening: PropTypes.func,
@@ -15,20 +16,27 @@ const propTypes = {
 const Dictaphone = ({
   transcript,
   resetTranscript,
+  finalTranscript,
   stopListening,
   browserSupportsSpeechRecognition,
 }) => {
   if (!browserSupportsSpeechRecognition) {
     return null;
   }
-  //const story = transcript.split(" ");
+  const stopRecording = () => {
+    stopListening();
+    const story = finalTranscript.split(" ");
+    resetTranscript();
+    console.log("STORY", story);
+    console.log("TRANSCRIPT", transcript);
+  };
   return (
     <div className="center">
       {/* <button onClick={resetTranscript}>
         Delete
       </button> */}
       <p>{transcript}</p>
-      <button className="stop-button" onClick={stopListening}>
+      <button className="stop-button" onClick={stopRecording}>
         STOP
       </button>
     </div>
